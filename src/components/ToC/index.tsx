a02dea2483b9e4f6b9eb062cbf2a187539934927
Wiktor Musial <wiktormusial@icloud.com>
2022-03-16 15:49:49 +0100

    Include table in main file

--- src/components/ToC/index.tsx
+++ src/components/ToC/index.tsx
@@ -1,10 +1,12 @@
 import ToCFilter from "./ToCFilter"
 import ToCPagination from "./ToCPagination"
+import ToCTable from "./ToCTable"
 
 const ToC = () => {
     return (
         <div>
             <ToCFilter />
+            <ToCTable />
             <ToCPagination />
         </div>
     )
