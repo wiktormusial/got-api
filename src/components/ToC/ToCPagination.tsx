ad9a1ac01fa05e0c40d36dab3c3a8b7ff06b9cf8
Wiktor Musial <wiktormusial@icloud.com>
2022-03-16 17:49:56 +0100

    Create pagination

--- src/components/ToC/ToCPagination.tsx
+++ src/components/ToC/ToCPagination.tsx
@@ -1,11 +1,36 @@
-const ToCPagination = () => {
+import React from "react"
+import { Reference } from "http-link-header"
+
+interface Props {
+    links: Reference[]
+    setUrl: React.Dispatch<React.SetStateAction<string>>
+}
+
+const ToCPagination: React.FC<Props> = ({ links, setUrl }) => {
     return (
-        <ul>
-            <li>Previous Page</li>
-            <li>First Page</li>
-            <li>Last Page</li>
-            <li>Next page</li>
-        </ul>
+        <>
+            <select
+                onChange={(e) =>
+                    setUrl(
+                        `https://anapioficeandfire.com/api/characters?page=1&pageSize=${e.target.value}`
+                    )
+                }
+            >
+                <option>Entries per page</option>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+            </select>
+            <ul>
+                {links.map((item) => {
+                    return (
+                        <li key={item.rel} onClick={() => setUrl(item.uri)}>
+                            {item.rel}
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
 
--- src/components/ToC/index.tsx
+++ src/components/ToC/index.tsx
@@ -1,32 +1,41 @@
-import axios from "axios"
 import { useEffect, useState } from "react"
+import axios from "axios"
+import Link, { Reference } from "http-link-header"
 import ToCFilter from "./ToCFilter"
 import ToCPagination from "./ToCPagination"
 import ToCTable from "./ToCTable"
 import { FetchCharacters } from "./types"
 
-const fetchCharacters = async () => {
-    const response = await axios.get<FetchCharacters[]>(
-        "https://anapioficeandfire.com/api/characters/"
-    )
+const fetchCharacters = async (url: string) => {
+    const response = await axios.get<FetchCharacters[]>(url)
     return response
 }
 
 const ToC = () => {
     const [data, setData] = useState<FetchCharacters[] | []>()
+    const [linkHeaders, setLinkHeaders] = useState<Reference[]>()
+    const [url, setUrl] = useState(
+        "https://anapioficeandfire.com/api/characters?page=1&pageSize=25"
+    )
     const [error, setError] = useState("")
 
     useEffect(() => {
-        fetchCharacters()
-            .then((res) => setData(res.data))
+        fetchCharacters(url)
+            .then((res) => {
+                setData(res.data)
+                setLinkHeaders(new Link(res.headers.link).refs)
+            })
             .catch((err) => setError(err))
-    }, [])
+    }, [url])
 
     return (
         <div>
             <ToCFilter />
+            {url}
             <ToCTable data={data} />
-            <ToCPagination />
+            {linkHeaders && (
+                <ToCPagination setUrl={setUrl} links={linkHeaders} />
+            )}
         </div>
     )
 }
