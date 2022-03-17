792743e9f4b7a37a1553068a2d62076c1a36a446
Wiktor Musial <wiktormusial@icloud.com>
2022-03-17 19:43:30 +0100

    Style pagination

--- src/components/ToC/ToCPagination.tsx
+++ src/components/ToC/ToCPagination.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { Reference } from "http-link-header"
+import { link } from "fs"
 
 interface Props {
     links: Reference[]
@@ -9,6 +10,14 @@ interface Props {
 
 const ToCPagination: React.FC<Props> = ({ setPagesize, links, setUrl }) => {
     const [lastPage, setLastPage] = useState<string | null>(null)
+
+    const getPageLink = (page: string) => {
+        const link = links.find((item) => item.rel === page)
+
+        if (link) {
+            setUrl(link.uri)
+        }
+    }
 
     useEffect(() => {
         links.forEach((item) => {
@@ -21,29 +30,53 @@ const ToCPagination: React.FC<Props> = ({ setPagesize, links, setUrl }) => {
 
     return (
         <>
-            <select
-                onChange={(e) => {
-                    setUrl(
-                        `https://anapioficeandfire.com/api/characters?page=1&pageSize=${e.target.value}`
-                    )
-                    setPagesize(parseInt(e.target.value))
-                }}
-            >
-                <option>Entries per page</option>
-                <option value="10">10</option>
-                <option value="25">25</option>
-                <option value="50">50</option>
-            </select>
-            <ul>
-                {links.map((item) => {
-                    return (
-                        <li key={item.rel} onClick={() => setUrl(item.uri)}>
-                            {item.rel}
+            <div className="pages">
+                <select
+                    onChange={(e) => {
+                        setUrl(
+                            `https://anapioficeandfire.com/api/characters?page=1&pageSize=${e.target.value}`
+                        )
+                        setPagesize(parseInt(e.target.value))
+                    }}
+                    className="form__select pages__element"
+                >
+                    <option>Entries per page</option>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                </select>
+                <p className="align-right pages__element">Pages: {lastPage}</p>
+            </div>
+            <div className="pagination">
+                <div className="pagination__element">
+                    <ul className="filters__pagination">
+                        <li
+                            className="filters__pagiation__element"
+                            onClick={() => getPageLink("prev")}
+                        >
+                            Previous page
                         </li>
-                    )
-                })}
-            </ul>
-            Pages: {lastPage}
+                        <li
+                            className="filters__pagiation__element"
+                            onClick={() => getPageLink("first")}
+                        >
+                            First page
+                        </li>
+                        <li
+                            className="filters__pagiation__element align-right"
+                            onClick={() => getPageLink("last")}
+                        >
+                            Last page
+                        </li>
+                        <li
+                            className="filters__pagiation__element align-right"
+                            onClick={() => getPageLink("next")}
+                        >
+                            Next page
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
