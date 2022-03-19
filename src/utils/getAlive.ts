920e1d35a8fd40edbc99aac623ea7efbc9de15d2
Wiktor Musial <wiktormusial@icloud.com>
2022-03-19 12:11:07 +0100

    Update getAlive

--- src/utils/getAlive.ts
+++ src/utils/getAlive.ts
@@ -6,7 +6,21 @@ const getAlive = (born: string, died: string) => {
     } else if (!died) {
         return "Yes"
     } else {
-        //todo
+        const yearOfBorn = born.split(" ")
+        const yearOfDied = died.split(" ")
+
+        if (!yearOfBorn.includes("BC") || !yearOfBorn.includes("BC,")) {
+            const yo = parseInt(yearOfDied[1]) - parseInt(yearOfBorn[1])
+
+            if (isNaN(yo)) {
+                return "Unknown"
+            } else {
+                return `No, died at ${yo} years old`
+            }
+      
+        } else {
+            return "Unknown"
+        }
     }
 }
 
